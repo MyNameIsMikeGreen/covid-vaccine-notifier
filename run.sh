f6e2f8f4ba17b1457a5c4dadf165fc78192fcfc1
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-05-21 23:15:33 +0100

    Autocreate venv

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+VENV_DIR=venv
+if [[ ! -d "$VENV_DIR" ]]; then
+    echo "$VENV_DIR directory not detected. Creating virtual environment..."
+    virtualenv ${VENV_DIR}
+fi
 source venv/bin/activate
 pip install -r requirements.txt
 python main.py
