0c66c013782ddc77fc984457cab6f7d3ed41b497
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-05-22 09:02:19 +0100

    Auto-switch working directory in run script

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
+echo "Performing environment setup..."
+ORIGINAL_DIRECTORY="`pwd`"
+LOCAL_DIRECTORY="`dirname ${0}`"
+cd ${LOCAL_DIRECTORY}
+LOG_FILE=./covidVaccineNotifier.log
+if [[ ! -f "$LOG_FILE" ]]; then
+    touch ${LOG_FILE}
+fi
+chmod -R 757 ${LOG_FILE}
 VENV_DIR=venv
 if [[ ! -d "$VENV_DIR" ]]; then
     echo "$VENV_DIR directory not detected. Creating virtual environment..."
@@ -5,5 +14,12 @@ if [[ ! -d "$VENV_DIR" ]]; then
 fi
 source venv/bin/activate
 pip install -r requirements.txt
+
+echo "Launching COVID Vaccine Notifier..."
 python main.py
+
+echo "Performing environment teardown..."
 deactivate
+cd ${ORIGINAL_DIRECTORY}
+echo "COVID Vaccine Notifier terminated."
+
